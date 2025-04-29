86e6fc84b48d2517f1f9d8aa08729f14dd469136
nehayaligar <nehayaligar03@gmail.com>
2025-04-29 15:50:21 +0530

    main branch

---
+++ Student2.java
@@ -0,0 +1,14 @@
+package q_two;
+
+public class Student2 {
+    
+        public static void main(String[] args){
+            String Name= "Madhu";
+            int birthyear=2005;
+            int currentyear=2025;
+            int age= currentyear-birthyear;
+            System.out.println("Name=" +Name);
+            System.out.println("Age=" +age);
+        }
+
+}
